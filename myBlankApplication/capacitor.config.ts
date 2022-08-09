4d0320306dd78f77037f3314f6fb399bcb9d4b49
jessicacfsb <jessica.cfsb@gmail.com>
2022-08-09 11:46:40 -0300

    Organizando arquivos

---
+++ myBlankApplication/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myBlankApplication',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
